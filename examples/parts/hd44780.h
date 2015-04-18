335c74d1fea91078709c7a01bb45d13bf843fbf5
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-04-18 14:40:07 +0200

    Reindented parts

--- examples/parts/hd44780.h
+++ examples/parts/hd44780.h
@@ -1,23 +1,23 @@
 /*
-	hd44780.h
+  hd44780.h
 
-	Copyright 2011 Michel Pollet <buserror@gmail.com>
+  Copyright 2011 Michel Pollet <buserror@gmail.com>
 
- 	This file is part of simavr.
+  This file is part of simavr.
 
-	simavr is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+  simavr is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-	simavr is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+  simavr is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with simavr.  If not, see <http://www.gnu.org/licenses/>.
- */
+  You should have received a copy of the GNU General Public License
+  along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
  * This "Part" simulates the business end of a HD44780 LCD display
@@ -50,88 +50,81 @@
 
 #include "sim_irq.h"
 
-enum {
-    IRQ_HD44780_ALL = 0,	// Only if (msb) RW:E:RS:D7:D6:D5:D4 (lsb)  configured
+enum
+  {
+    IRQ_HD44780_ALL = 0,   // Only if (msb) RW:E:RS:D7:D6:D5:D4 (lsb)  configured
     IRQ_HD44780_RS,
     IRQ_HD44780_RW,
     IRQ_HD44780_E,
     // bidirectional
-    IRQ_HD44780_D0,IRQ_HD44780_D1,IRQ_HD44780_D2,IRQ_HD44780_D3,
-    IRQ_HD44780_D4,IRQ_HD44780_D5,IRQ_HD44780_D6,IRQ_HD44780_D7,
+    IRQ_HD44780_D0, IRQ_HD44780_D1, IRQ_HD44780_D2, IRQ_HD44780_D3,
+    IRQ_HD44780_D4, IRQ_HD44780_D5, IRQ_HD44780_D6, IRQ_HD44780_D7,
     IRQ_HD44780_INPUT_COUNT,
 
-    IRQ_HD44780_BUSY = IRQ_HD44780_INPUT_COUNT,	// for VCD traces sake...
+    IRQ_HD44780_BUSY = IRQ_HD44780_INPUT_COUNT,   // for VCD traces sake...
     IRQ_HD44780_ADDR,
     IRQ_HD44780_DATA_IN,
     IRQ_HD44780_DATA_OUT,
     IRQ_HD44780_COUNT
-};
+  };
 
-enum {
-    HD44780_FLAG_F = 0,         // 1: 5x10 Font, 0: 5x7 Font
-    HD44780_FLAG_N,             // 1: 2/4-lines Display, 0: 1-line Display,
-    HD44780_FLAG_D_L,           // 1: 4-Bit Interface, 0: 8-Bit Interface
-    HD44780_FLAG_R_L,           // 1: Shift right, 0: shift left
-    HD44780_FLAG_S_C,           // 1: Display shift, 0: Cursor move
-    HD44780_FLAG_B,             // 1: Cursor Blink
-    HD44780_FLAG_C,             // 1: Cursor on
-    HD44780_FLAG_D,             // 1: Set Entire Display memory (for clear)
-    HD44780_FLAG_S,             // 1: Follow display shift
-    HD44780_FLAG_I_D,			// 1: Increment, 0: Decrement
+enum
+  {
+    HD44780_FLAG_F = 0,   // 1: 5x10 Font, 0: 5x7 Font
+    HD44780_FLAG_N,   // 1: 2/4-lines Display, 0: 1-line Display,
+    HD44780_FLAG_D_L,   // 1: 4-Bit Interface, 0: 8-Bit Interface
+    HD44780_FLAG_R_L,   // 1: Shift right, 0: shift left
+    HD44780_FLAG_S_C,   // 1: Display shift, 0: Cursor move
+    HD44780_FLAG_B,   // 1: Cursor Blink
+    HD44780_FLAG_C,   // 1: Cursor on
+    HD44780_FLAG_D,   // 1: Set Entire Display memory (for clear)
+    HD44780_FLAG_S,   // 1: Follow display shift
+    HD44780_FLAG_I_D,   // 1: Increment, 0: Decrement
 
     /*
      * Internal flags, not HD44780
      */
-    HD44780_FLAG_LOWNIBBLE,		// 1: 4 bits mode, write/read low nibble
-    HD44780_FLAG_BUSY,			// 1: Busy between instruction, 0: ready
-    HD44780_FLAG_REENTRANT,		// 1: Do not update pins
+    HD44780_FLAG_LOWNIBBLE,   // 1: 4 bits mode, write/read low nibble
+    HD44780_FLAG_BUSY,   // 1: Busy between instruction, 0: ready
+    HD44780_FLAG_REENTRANT,   // 1: Do not update pins
 
-    HD44780_FLAG_DIRTY,			// 1: needs redisplay...
-    HD44780_FLAG_CRAM_DIRTY,	// 1: Character memory has changed
-};
+    HD44780_FLAG_DIRTY,   // 1: needs redisplay...
+    HD44780_FLAG_CRAM_DIRTY,   // 1: Character memory has changed
+  };
 
 
 typedef struct hd44780_t
 {
-	avr_irq_t * irq;
-	struct avr_t * avr;
-	int		w, h;				// width and height of the LCD
+  avr_irq_t *irq;
+  struct avr_t *avr;
+  int w, h;   // width and height of the LCD
 
-	uint16_t cursor;			// offset in vram
-	uint8_t  vram[80 + 64];
+  uint16_t cursor;   // offset in vram
+  uint8_t vram[80 + 64];
 
-	uint16_t pinstate;			// 'actual' LCd data pins (IRQ bit field)
-	// uint16_t oldstate;			/// previous pins
-	uint8_t	 datapins;			// composite of 4 high bits, or 8 bits
-	uint8_t  readpins;
+  uint16_t pinstate;   // 'actual' LCd data pins (IRQ bit field)
+  // uint16_t oldstate;   /// previous pins
+  uint8_t datapins;   // composite of 4 high bits, or 8 bits
+  uint8_t readpins;
 
-	uint16_t flags;				// LCD flags ( HD44780_FLAG_*)
+  uint16_t flags;   // LCD flags ( HD44780_FLAG_*)
 } hd44780_t;
 
-void
-hd44780_init(
-		struct avr_t *avr,
-		struct hd44780_t * b,
-		int width,
-		int height );
-void
-hd44780_print(
-		struct hd44780_t *b);
+void hd44780_init (struct avr_t *avr, struct hd44780_t *b, int width, int height);
+void hd44780_print (struct hd44780_t *b);
 
 static inline int
-hd44780_set_flag(
-		hd44780_t *b, uint16_t bit, int val)
+hd44780_set_flag (hd44780_t * b, uint16_t bit, int val)
 {
-	int old = b->flags &  (1 << bit);
-	b->flags = (b->flags & ~(1 << bit)) | (val ? (1 << bit) : 0);
-	return old != 0;
+  int old = b->flags & (1 << bit);
+  b->flags = (b->flags & ~(1 << bit)) | (val ? (1 << bit) : 0);
+  return old != 0;
 }
 
 static inline int
-hd44780_get_flag(
-		hd44780_t *b, uint16_t bit)
+hd44780_get_flag (hd44780_t * b, uint16_t bit)
 {
-	return (b->flags &  (1 << bit)) != 0;
+  return (b->flags & (1 << bit)) != 0;
 }
 
-#endif 
+#endif
--- examples/parts/i2c_eeprom.h
+++ examples/parts/i2c_eeprom.h
@@ -1,23 +1,23 @@
 /*
-	i2c_eeprom.h
+  i2c_eeprom.h
 
-	Copyright 2008, 2009 Michel Pollet <buserror@gmail.com>
+  Copyright 2008, 2009 Michel Pollet <buserror@gmail.com>
 
- 	This file is part of simavr.
+  This file is part of simavr.
 
-	simavr is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+  simavr is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-	simavr is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+  simavr is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with simavr.  If not, see <http://www.gnu.org/licenses/>.
- */
+  You should have received a copy of the GNU General Public License
+  along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 #ifndef __I2C_EEPROM_H___
@@ -39,18 +39,19 @@
  *
  * these two modes seem to cover many eeproms
  */
-typedef struct i2c_eeprom_t {
-	avr_irq_t *	irq;		// irq list
-	uint8_t addr_base;
-	uint8_t addr_mask;
-	int verbose;
+typedef struct i2c_eeprom_t
+{
+  avr_irq_t *irq;   // irq list
+  uint8_t addr_base;
+  uint8_t addr_mask;
+  int verbose;
 
-	uint8_t selected;		// selected address
-	int index;	// byte index in current transaction
+  uint8_t selected;   // selected address
+  int index;   // byte index in current transaction
 
-	uint16_t reg_addr;		// read/write address register
-	int size;				// also implies the address size, one or two byte
-	uint8_t ee[4096];
+  uint16_t reg_addr;   // read/write address register
+  int size;   // also implies the address size, one or two byte
+  uint8_t ee[4096];
 } i2c_eeprom_t;
 
 /*
@@ -70,22 +71,13 @@ typedef struct i2c_eeprom_t {
  * The "data" is optional, data is initialized as 0xff like a normal eeprom.
  */
 void
-i2c_eeprom_init(
-		struct avr_t * avr,
-		i2c_eeprom_t * p,
-		uint8_t addr,
-		uint8_t mask,
-		uint8_t * data,
-		size_t size);
+i2c_eeprom_init (struct avr_t *avr,
+                 i2c_eeprom_t * p, uint8_t addr, uint8_t mask, uint8_t * data, size_t size);
 
 /*
  * Attach the eeprom to the AVR's TWI master code,
  * pass AVR_IOCTL_TWI_GETIRQ(0) for example as i2c_irq_base
  */
-void
-i2c_eeprom_attach(
-		struct avr_t * avr,
-		i2c_eeprom_t * p,
-		uint32_t i2c_irq_base );
+void i2c_eeprom_attach (struct avr_t *avr, i2c_eeprom_t * p, uint32_t i2c_irq_base);
 
 #endif /* __I2C_EEPROM_H___ */
